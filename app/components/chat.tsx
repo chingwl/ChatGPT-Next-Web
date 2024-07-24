b16d7e397a7e45a5714b8630a30fca34cdf1503b
jingwl <jingwl@youyuan.com>
2024-07-24 21:27:41 +0800

    fix: update chat message date display in chat.tsx

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -1489,7 +1489,10 @@ function _Chat() {
                   <div className={styles["chat-message-action-date"]}>
                     {isContext
                       ? Locale.Chat.IsContext
-                      : message.model?.toLocaleString() + ' - ' + message.date.toLocaleString()}
+                      : isUser 
+                      ? message.date.toLocaleString()
+                      : message.model?.toLocaleString() + ' - ' + message.date.toLocaleString()
+                    }
                   </div>
                 </div>
               </div>
