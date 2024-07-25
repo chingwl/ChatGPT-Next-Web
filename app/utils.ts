ca5f39e82e24c8569a30b6e7480f3885c170ac5e
jingwl <jingwl@youyuan.com>
2024-07-25 12:15:26 +0800

    feat: Add support for new model "kimi"

--- app/utils.ts
+++ app/utils.ts
@@ -257,6 +257,7 @@ export function isVisionModel(model: string) {
     "gemini-1.5-flash",
     "gpt-4o",
     "gpt-4o-mini",
+    "kimi"
   ];
   const isGpt4Turbo =
     model.includes("gpt-4-turbo") && !model.includes("preview");
